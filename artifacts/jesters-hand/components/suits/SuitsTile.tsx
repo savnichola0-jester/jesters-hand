2e8195de06a44e55e3dcedfd988d544fcd181ffc
savnichola0-jester <s.a.v.nichola0@gmail.com>
2026-09-02 00:20:20 -0600

    Show transparent SUITS tile artwork

--- artifacts/jesters-hand/components/suits/SuitsTile.tsx
+++ artifacts/jesters-hand/components/suits/SuitsTile.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Image } from 'react-native';
 
 export default function SuitsTile({ size }: { size: number }) {
   return (
     <Image
       source={require('../../assets/images/icon_suits.png')}
-      style={[s.image, { width: size, height: size }]}
+      style={{ width: size, height: size }}
       resizeMode="contain"
       accessibilityLabel="SUITS"
     />
   );
 }
-
-const s = StyleSheet.create({
-  image: { backgroundColor: '#000' },
-});
