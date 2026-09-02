3849fb6ed866e53067a185e1978fcabc88ae05c0
savnichola0-jester <s.a.v.nichola0@gmail.com>
2026-09-01 18:04:07 -0600

    Fix EAS Android Metro compatibility

--- artifacts/jesters-hand/metro.config.js
+++ artifacts/jesters-hand/metro.config.js
@@ -1,3 +1,13 @@
+if (typeof Array.prototype.toReversed !== 'function') {
+  Object.defineProperty(Array.prototype, 'toReversed', {
+    configurable: true,
+    writable: true,
+    value() {
+      return this.slice().reverse();
+    },
+  });
+}
+
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = getDefaultConfig(__dirname);
